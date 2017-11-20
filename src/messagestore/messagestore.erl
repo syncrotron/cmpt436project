3c4bad3b4c915719094b67e5cf2597ad4f18d4a3
Fredrik Johansson <f95johansson@gmail.com>
2017-11-21 00:42:01 +0100

    Started with messagestore

---
+++ src/messagestore/messagestore.erl
@@ -0,0 +1,15 @@
+-module(messagestore).
+-include("messagestore.hrl").
+
+%-behaviour(gen_server).
+
+%start() -> 
+%    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+%init([]) ->
+%    Store = dict:new(),
+%    {ok, Store}.
+
+%get_message() -> ok.
+%add_message() -> ok.
+%flag_message_for_deletion() -> ok.

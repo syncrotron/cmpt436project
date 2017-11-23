de3d084ef49d9bcc8b2934de8daadcf096436801
sch923 <s.horovatin@usask.ca>
2017-11-22 22:35:23 -0600

    domian table func and testing file
    -added basic table interaction functions
    -added empty test suite

---
+++ ct/demo/domaintable_SUITE.erl
@@ -0,0 +1,8 @@
+%%%Created by Sam Horovatin
+
+-module(domaintable_SUITE).
+-include_lib("common_test/include/ct.hrl").
+
+-export([all/0]).
+
+all() -> []
--- tables/domaintable.erl
+++ tables/domaintable.erl
@@ -5,6 +5,8 @@
 -include("../src/domaintable.hrl").
 -export([init/0]).
 
+
+%%% init: intizalizes table on system boot
 init() ->
     application:set_env(mnesia, dir, ?DomaintableDB),   %%%Sets directory to save database in
     ok =  mnesia:create_schema([node()]),
@@ -13,10 +15,21 @@ init() ->
                       [{attributes, record_info(fields, object)},
                        {index, [#object.id]},
                        {disc_copies, [node()]},       %%%Stores a copies on RAM and on disc
-                       {type, bag}]).                %%%Allows for multiple unique entries under 1 key
+                       {type, bag}]).                 %%%Allows for multiple unique entries under 1 key. Managed by table handler
 
 
 
+
+
+%%%%%%%%%%%%%%%
+%%% PRIVATE %%%
+%%%%%%%%%%%%%%%
+
+
+%%% insert_object: Adds an object to the table via transaction
+%%%      Id: 64 bit unique address, reminicent of IP address to identify objects universally
+%%%      Position: a 3d ecludian tuple, consisting of X, Y, and Z values
+%%%      Delta_pos: a kelper problem equation that solves for future object position based on last know position and associatve time stamp
 insert_object(Id, Position, Delta_pos) ->
     ObjTrans = fun() ->
                   mnesia:write(#object{
@@ -26,4 +39,26 @@ insert_object(Id, Position, Delta_pos) ->
                     last_msg_t_stamp=erlang:timestamp()
                   })
                end,
+    mnesia:activity(transaction, ObjTrans).
+
+%%% object_by_Id: Returns a list of records matching the given ID
+%%%      Id: 64 bit unique address, reminicent of IP address to identify objects universally
+object_by_Id(Id) ->
+    ObjTrans = fun() ->
+                  mnesia:read({domaintable, Id})
+               end,
+    mnesia:activity(transaction, ObjTrans).
+
+%%% object_by_position: Returns a lsit of records matching the given position
+%%%      Position: a 3d ecludian tuple, consisting of X, Y, and Z values
+object_by_position(Position) ->
+    ObjTrans = fun() ->
+                  mnesia:match_object(domaintable, {object, '_' , Position , '_' , '_' }, read)
+               end,
+    mnesia:activity(transaction, ObjTrans).
+
+%%% object_by_Id: Deletes an entire object history from the table
+%%%      Id: 64 bit unique address, reminicent of IP address to identify objects universally
+remove_object(Id) ->
+    ObjTrans = fun() -> mnesia:delete({domaintable, Id}) end,
     mnesia:activity(transaction, ObjTrans).
